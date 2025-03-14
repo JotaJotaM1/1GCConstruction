1aad0613ada8fede94ed876739970d2b29d8d62c
JotaJotaM1 <johnjames_89@hotmail.com>
2025-03-14 15:10:33 -0500

    first section services finished titlle and img grid

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -3,8 +3,19 @@ import "../styles/pages/Services.css";
 function Services() {
     return (
         <main>
-            <h1>Bienvenido a la página de Services</h1>
-            <p>Este es el contenido de la página de Services.</p>
+            <section>
+                <div>
+                    <h1 className="titleH1Services">Our Services</h1>
+                    <p className="textH1Services">We begin by understanding your vision and translating it into creative, functional design concepts, Helping you choose the perfect design style to reflect your personality and needs.</p>
+                </div>
+            </section>
+            <section>
+                <div className="grid-container">
+                    <div className="grid-item item1"></div>
+                    <div className="grid-item item2"></div>
+                    <div className="grid-item item3"></div>
+                </div>
+            </section>
         </main>
     );
 }
