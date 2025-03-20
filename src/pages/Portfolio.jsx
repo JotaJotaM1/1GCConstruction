54bed95601754f9e5395e336b9b9ca91d23c9124
JotaJotaM1 <johnjames_89@hotmail.com>
2025-03-20 14:41:38 -0500

    Partially implemented portfolio page with navigable tabs, manual carousel, and hero section containing h1

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,10 +1,42 @@
 import "../styles/pages/Portfolio.css";
+import ManualCarousel from "../components/ManualCarousel";
+import TabsNavegation from "../components/TabsNavegation";
+import Arquite1 from "../assets/arquite1.jpg";
+import CtaAll from '../components/CtaAll';
 
 function Portfolio() {
     return (
         <main>
-            <h1>Bienvenido a la página de Portfolio</h1>
-            <p>Este es el contenido de la página de Portfolio.</p>
+            <div>
+                <section>
+                    <div className="heroContainerH1">
+                        {/* Imágenes de fondo */}
+                        <div className="background-images">
+                            <img src={Arquite1} alt="Background 1" className="background-image" />
+                        </div>
+
+                        {/* Título */}
+                        <div className="hero-text">
+                            <h1>Elevate Your Style</h1>
+                            <p>with Precision</p>
+                        </div>
+                    </div>
+                </section>
+
+                <section>
+                    <ManualCarousel />
+                </section>
+                <section>
+                    <h2 className="tittleResourH2">Our Projects</h2>
+                    <p className="textTittleResourH2">We begin by understanding your vision and translating it into creative, functional design concepts, Helping you choose the perfect design style to reflect your personality and needs.</p>
+                </section>
+                <section>
+                    <TabsNavegation />
+                </section>
+                <section>
+                    <CtaAll />
+                </section>
+            </div>
         </main>
     );
 }
--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -1,17 +1,11 @@
 import "../styles/pages/Resources.css";
-import ManualCarousel from "../components/ManualCarousel";
 
 function Resources() {
     return (
         <main>
             <div>
-                <section>
-                    <h1 className="tittleResourH1">Our Projects</h1>
-                    <p className="textTittleResourH1">We begin by understanding your vision and translating it into creative, functional design concepts, Helping you choose the perfect design style to reflect your personality and needs.</p>
-                </section>
-                <section>
-                    <ManualCarousel />
-                </section>
+                    <h1 className="tittleResourH1">Our Resources</h1>
+                    <p className="textTittleResourH1">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus reiciendis, dicta architecto nisi consequuntur itaque ducimus quibusdam unde. Vel quae quaerat molestias sit obcaecati illum veritatis, voluptates accusamus doloribus animi!</p>
             </div>
         </main>
     );
